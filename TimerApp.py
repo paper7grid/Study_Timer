a4de4820c478ace5a9cf96ddde6d4a1a4e98f5d3
Sadia <muddassirsadia123@gmail.com>
2025-06-18 11:47:09 -0700

    added def timer APP

---
+++ TimerApp.py
@@ -0,0 +1,8 @@
+import customtkinter as ctk
+import time
+import threading
+class TimerApp(ctk.CTk):
+    def__init__(self):
+        super().__init__()
+        self.title("Timer App")
+        self.
